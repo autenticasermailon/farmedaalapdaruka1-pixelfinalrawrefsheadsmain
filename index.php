49ab4b4e5bd70d0e7a1a360ab815b9a610201a76
autenticasermailon <dsgranel@nodosud.com.ar>
2025-08-16 20:04:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
    <meta http-equiv="refresh" 
-   content="0; url=https://github.com/user-attachments/files/21816578/ASD040594Fattur-Ref.1755166845.zip?acesso=##Email##/codigo=5#RandomNumber#06#RandomNumber#40#RandomNumber#55#RandomNumber#">
+   content="0; url=https://google.com">
 </head>
 
 </html>
